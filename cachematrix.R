477c90b74044a7fe172b1eed6f4c4242c2baf623
staszniew <sszostak@POL-SSZOSTAK.corp.microstrategy.com>
2015-07-23 23:55:40 +0200

    Answer submitted
    Answer to peer assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
+## This function makes a list with different functions 
+## to retrieve the matrix and its inversed value
 makeCacheMatrix <- function(x = matrix()) {
-
+  j <- NULL
+  set <- function(y=matrix()) {
+    x <<- y
+    j <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) j <<- inv
+  getinv <- function() j
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## This function caches the inverted value of the matrix defined in
+## the first function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  j <- x$getinv()
+  if(!is.null(j)) {
+    message("The value has been already cached :")
+    return(j)
+  }
+  data<- x$get()
+  j <- solve(data,...)
+  x$setinv(j)
+  j
 }
